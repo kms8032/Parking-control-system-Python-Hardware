d7d1692baea9673e7bf2d90c9163c71ccc09e671
KIM 1412 <alstjr803232@g.yju.ac.kr>
2025-09-23 11:14:55 +0900

    feat: Express 연결

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -1,5 +1,6 @@
 import time
 import platform
+import socketio
 
 from flask import Flask, render_template, Response, stream_with_context
 from flask_socketio import SocketIO, emit
@@ -9,6 +10,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+express_sio = socketio.Client()
+express_sio.connect('http://localhost:3000')
+
+@express_sio.event
+def connect():
+    print('Connected to Express server')
+
+@express_sio.event
+def disconnect():
+    print('Disconnected from Express server')
 
 # 클라이언트에서 연결되었을 때 처리하는 이벤트
 @socketio.on('connect')
@@ -22,6 +34,7 @@ def handle_message(data):
     print('Received message:', data)
     # 받은 데이터를 처리하거나, 필요에 따라 클라이언트에게 다시 전송할 수 있습니다.
     emit('data', {'data': data}, broadcast=True)   # broadcast=True로 설정하면 연결된 모든 클라이언트에게 전송
+    express_sio.emit("ai_result", data)
 
 # 클라이언트가 연결 해제되었을 때 처리하는 이벤트
 @socketio.on('disconnect')
