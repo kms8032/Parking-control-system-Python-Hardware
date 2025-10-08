420d775d138b28b3e3f2a5364148ee4286bd7134
KIM 1412 <alstjr803232@g.yju.ac.kr>
2025-10-08 09:53:47 +0900

    docs: 주석 보완 및 코드 이해도 개선

--- DisplayController/RPI_display_socket_connetion.py
+++ DisplayController/RPI_display_socket_connetion.py
@@ -11,12 +11,9 @@ def disconnect():
     print("disconnect")
 
 @sio.on('rpi_data')
-#def catch_all(event, data=None):
-   # print(f"Data: {event}, data: {data}")
-   # print("Receive Data: ", data)
-    # 방향 디스플레이 표기 코드 구
 def data(data):
     print(f"data: {data}")
+    # 방향 디스플레이 구현 코드
 
 sio.connect("http://192.168.0.20:5002")
 
