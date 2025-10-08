39fcf3597c97898fa423e3010bd0abab6b3fdd8c
kms8032 <alstjr803232@g.yju.ac.kr>
2025-10-07 15:40:35 -0900

    fix: update server conncetion address

--- DisplayController/RPI_display_socket_connetion.py
+++ DisplayController/RPI_display_socket_connetion.py
@@ -2,9 +2,22 @@ import socketio
 
 sio = socketio.Client()
 
-@sio.on('rpi_data')
-def display_information(data):
-    print("Receive Data: ", data)
-    # 방향 디스플레이 표기 코드 구현
+@sio.event()
+def connect():
+    print("connect")
 
-sio.connect("127.0.0.1:5002")
+@sio.event()
+def disconnect():
+    print("disconnect")
+
+@sio.on('rpi_data')
+#def catch_all(event, data=None):
+   # print(f"Data: {event}, data: {data}")
+   # print("Receive Data: ", data)
+    # 방향 디스플레이 표기 코드 구
+def data(data):
+    print(f"data: {data}")
+
+sio.connect("http://192.168.0.20:5002")
+
+sio.wait()
